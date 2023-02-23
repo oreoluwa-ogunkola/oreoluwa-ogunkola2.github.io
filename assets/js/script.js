59c1f4c902a604320f5adabfc4178eaba39bfa41
Oreoluwa Ogunkola <100085594+oreoluwa-ogunkola@users.noreply.github.com>
2023-02-23 13:48:53 +0000

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,10 @@
 var currentImage = document.getElementById("current-image");
-
-
 var imageThumbs = document.getElementById("image-thumbs");
-
-for (var i = 1; i <= 10; i++) {
-  thumb.addEventListener(
-  "click", function() {
-
-  }
-);
-
-}
-var thumb = document.createElement("img");
 
 thumb.classList.add("thumb");
 
-thumb.src = "images/image" + i + ".jpg";
-thumb.alt = "Image " + i;
+for (var i = 1; i <= 10; i++) {
+  var thumb = document.createElement("img");
+  thumb.src = "visual/visual" + i + ".png";
+  thumb.alt = "Image " + i;
+}
