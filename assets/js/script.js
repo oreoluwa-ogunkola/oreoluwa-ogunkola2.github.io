14365bc9fa18ffc2b21140b33a67d35d5a3d8d67
Oreoluwa Ogunkola <100085594+oreoluwa-ogunkola@users.noreply.github.com>
2023-02-23 14:32:51 +0000

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,14 @@
 var currentImage = document.getElementById("current-image");
 var imageThumbs = document.getElementById("image-thumbs");
 
- thumb.classList.add("thumb");
-
 for (var i = 1; i <= 10; i++) {
   var thumb = document.createElement("img");
- 
+  thumb.src ="visual/visual" + i + ".jpg";
+  thumb.alt ="visual " + i;
+  thumb.classList.add("thumb");
+  imageThumbs.appendChild(thumb);
   thumb.addEventListener(
-  "click", function() {
+   "click", function() {
     currentImage.src = this.src;
     currentImage.alt = this.alt;
   
